949b7994dd4c00575d7f2f5cdb146dbf4e44f9be
jangkyoungtae <zxs3710@gmail.com>
2025-01-14 19:36:56 +0900

    delete:사용하지 않는 코드 제거

--- app/(auth)/signup.tsx
+++ app/(auth)/signup.tsx
@@ -1,6 +1,5 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet ,Text, View } from 'react-native';
 
-import { Text, View } from '@/components/Themed';
 
 export default function SignUp() {
   return (
